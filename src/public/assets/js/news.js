06e070d3ad43ef78e44167dff5413b999884541c
chesteraustin <chesteraustin@gmail.com>
2017-08-18 13:52:54 -0700

    initial scrape

--- src/public/assets/js/news.js
+++ src/public/assets/js/news.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 	//Document loaded, create event listeners
 	console.log("document loaded")
+	$("#getNews").on("click", function(){
+		getNews();
+	})
 });
+
+function getNews(){
+	$.ajax({
+		"method": "POST",
+		"url": "/news/google/"
+	})
+	.done(function(response) {
+		console.log(response);
+	})
+	.error(function(err){
+		console.log(err);
+	});
+}
--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -2,9 +2,45 @@
 // =============================================================
 var path = require("path");
 var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
+var mongojs = require("mongojs");
 var router = express.Router();
 
-//Read a project
+//Set up MongoDB
+var databaseUrl = "newsFeed";
+var collections = ["news"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+//Read news
+router.post("/news/google", function(req, res) {
+  var url = "https://news.google.com/news/?ned=us&hl=en";
+  request(url, function(error, response, html) {
+
+	// Load the HTML into cheerio and save it to a variable
+	// '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+	var $ = cheerio.load(html);
+
+		$(".hzdq5d").each(function(i, element) {
+			var title = $(element).text();
+			var link = $(element).attr("href");
+			db.news.insert({
+				"url": url,
+				"title": title,
+				"link": link,
+				"retrieval": new Date()
+			});
+		});
+	});
+
+	res.json("got google news")
+});
+
 router.get("/api/get-news", function(req, res) {
 	res.json(error)
 });
